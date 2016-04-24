53dd1ae0d20e18551551170dcd61f11f372f5b35
JTK222 <lukasporwit3@googlemail.com>
2016-04-24 09:22:50 +0200

    Updated Version Check File

--- src/main/java/net/drpcore/common/util/DRPCoreInfo.java
+++ src/main/java/net/drpcore/common/util/DRPCoreInfo.java
@@ -5,9 +5,9 @@ public class DRPCoreInfo {
 
 	public static final String MODID = "drpcore";
 	public static final String NAME = "Dark Roleplay Core";
-	public static final String VERSION = "0.1.4";
+	public static final String VERSION = "0.1.4-PRE";
 	public static final String ACCEPTEDVERSIONS = "[1.9]";
 	
-	public static final String UPDATECHECK = "https://dl.dropboxusercontent.com/content_link/jwEUGVQgCsTtQma6UW7wnntGGThzuttY9Z89BY3bJSK5Qi6EQ032KJtHzx6v0em4/file";
+	public static final String UPDATECHECK = "https://raw.githubusercontent.com/DarkRoleplay/1.9-Dark-Roleplay-Core/master/DarkRoleplayCore.json";
 	
 }
