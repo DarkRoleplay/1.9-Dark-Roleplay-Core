dcd929528cb3ef0305a911ee694256aa83cf4b09
JTK222 <lukasporwit3@googlemail.com>
2016-09-23 20:40:40 +0200

    Update 0.1.6b

--- src/main/java/net/drpcore/common/util/DRPCoreInfo.java
+++ src/main/java/net/drpcore/common/util/DRPCoreInfo.java
@@ -5,9 +5,9 @@ public class DRPCoreInfo {
 
 	public static final String NAME = "Dark Roleplay Core";
 
-	public static final String VERSION = "0.1.6";
+	public static final String VERSION = "0.1.6b";
 
-	public static final String ACCEPTEDVERSIONS = "[1.9,)";
+	public static final String ACCEPTEDVERSIONS = "[1.9.4,)";
 
 	public static final String UPDATECHECK = "https://raw.githubusercontent.com/DarkRoleplay/1.9-Dark-Roleplay-Core/master/DarkRoleplayCore.json";
 }
