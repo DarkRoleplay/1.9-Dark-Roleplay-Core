98b9f34fa6e3f460347c4b8dd753030e4299d19a
JTK222 <lukasporwit3@googlemail.com>
2016-12-07 18:59:27 +0100

    ItemMainClass

---
+++ src/main/java/net/dark_roleplay/drpcore/common/items/DRPCoreItems.java
@@ -0,0 +1,47 @@
+package net.dark_roleplay.drpcore.common.items;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class DRPCoreItems {
+
+	/**---------- A ----------**/
+	/**---------- B ----------**/
+	/**---------- C ----------**/
+	/**---------- D ----------**/
+	/**---------- E ----------**/
+	/**---------- F ----------**/
+	/**---------- G ----------**/
+	/**---------- H ----------**/
+	/**---------- I ----------**/
+	/**---------- J ----------**/
+	/**---------- K ----------**/
+	/**---------- L ----------**/
+	/**---------- M ----------**/
+	/**---------- N ----------**/
+	/**---------- O ----------**/
+	/**---------- P ----------**/
+	/**---------- Q ----------**/
+	/**---------- R ----------**/
+	/**---------- S ----------**/
+	/**---------- T ----------**/
+	/**---------- U ----------**/
+	/**---------- V ----------**/
+	/**---------- W ----------**/
+	/**---------- X ----------**/
+	/**---------- Y ----------**/
+	/**---------- Z ----------**/
+	
+	public static final void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	public static final void init(FMLInitializationEvent event) {}
+
+	public static final void postInit(FMLPostInitializationEvent event) {}
+
+	public static final void registerItem(DRPItem item){
+		
+	}
+}
