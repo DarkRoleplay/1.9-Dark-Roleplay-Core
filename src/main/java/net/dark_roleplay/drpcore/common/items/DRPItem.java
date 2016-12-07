0625ad0bf0b674affa90855da4cdb2aac6854b05
DaNeubi <Lukas.Neubauer97@hotmail.de>
2016-12-07 18:59:31 +0100

    First attempts

---
+++ src/main/java/net/dark_roleplay/drpcore/common/items/DRPItem.java
@@ -0,0 +1,14 @@
+package net.dark_roleplay.drpcore.common.items;
+
+import net.minecraft.item.Item;
+
+public class DRPItem extends Item{
+
+	//Method for 
+	private final String textures;
+	
+	
+	public DRPItems (){
+		
+	}
+}
---
+++ src/main/java/net/dark_roleplay/drpcore/common/items/consumable/medicine/MedicineBase.java
@@ -0,0 +1,8 @@
+package net.dark_roleplay.drpcore.common.items.consumable.medicine;
+
+import net.minecraft.item.Item;
+
+public class MedicineBase extends Item{
+	
+	
+}
